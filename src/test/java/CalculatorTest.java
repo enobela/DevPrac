6bc5db801a73e2a9340fda970c3813009f629ff1
Ephraim <nobelapro@gmail.com>
2025-05-21 10:02:30 +0000

    Merge github/main into main
    Conflicts:
    	pom.xml

---
+++ src/test/java/CalculatorTest.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/UnitTests/JUnit5TestClass.java to edit this template
+ */
+
+import com.mycompany.calcapp.Calculator;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ *
+ * @author RC_Student_lab
+ */
+public class CalculatorTest {
+    
+    @Test
+    public void testAdd() {
+        Calculator calculator = new Calculator();
+        assertEquals(5, calculator.add(2, 3));
+    }
+    
+}
